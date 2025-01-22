011b3c10f98c46939302255b145467bf43e54009
frandd07 <fran.doblado.diaz@gmail.com>
2025-01-22 12:56:16 +0100

    Update

--- src/app/cuadrado/Cuadrado.js
+++ src/app/cuadrado/Cuadrado.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
-export default function Cuadrado(){
-  const [estaClicado, setEstaClicado] = useState(false)
+export default function Cuadrado() {
+  const [estaClicado, setEstaClicado] = useState(false);
 
   const manejarClic = () => {
     setEstaClicado(!estaClicado);
@@ -9,12 +9,12 @@ export default function Cuadrado(){
 
   return (
     <div className="contenedor">
+      <div className="circulo-detras"></div> {/* Círculo detrás del cuadrado */}
       <div
         className={`cuadrado ${estaClicado ? 'clicado' : ''}`}
         onClick={manejarClic}
-      >
-
-      </div>
+      ></div>
+      <div className="circulo-delante"></div> {/* Círculo delante del cuadrado */}
     </div>
   );
-};
+}
